e0862b6882aaf5b526469fea48e8ff81b633526e
salva <salva@LAPTOP-HN46D38M>
2023-02-23 10:56:47 +0100

    Commit 2

--- src/main/java/com/mycompany/endecalculadora/Calculadora.java
+++ src/main/java/com/mycompany/endecalculadora/Calculadora.java
@@ -10,4 +10,30 @@ package com.mycompany.endecalculadora;
  */
 public class Calculadora {
     private int num1, num2, resul;
+
+    public int getNum1() {
+        return num1;
+    }
+
+    public void setNum1(int num1) {
+        this.num1 = num1;
+    }
+
+    public int getNum2() {
+        return num2;
+    }
+
+    public void setNum2(int num2) {
+        this.num2 = num2;
+    }
+
+    public int getResul() {
+        return resul;
+    }
+
+    public void setResul(int resul) {
+        this.resul = resul;
+    }
+    
+    
 }
